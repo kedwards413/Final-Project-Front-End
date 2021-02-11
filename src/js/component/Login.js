448902785ca7e261dd76d2e20d445d9f44eafd82
kedwards413 <kaela.rose.edwards@gmail.com>
2021-02-11 17:37:58 +0000

    updates

---
+++ src/js/component/Login.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export const Login = () => {
+    return (
+    <>
+<div className="login-box">
+  <h2>Login</h2>
+  <form>
+    <div className="user-box">
+      <input type="text" name="" required=""></input>
+      <label>Username</label>
+    </div>
+    <div className="user-box">
+      <input type="password" name="" required=""></input>
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+</div>
+</>
+    );
+};
