ea80b9f34901f2f68fb2826c08cd99b5640715d0
Outdoor-Dev <saltairpix@gmail.com>
2021-02-15 03:53:43 +0000

    update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -10,10 +10,10 @@ export const Home = () => {
 
 	return (
 		<>
-			<img
+			{/* <img
 				className="background-image"
 				src="https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1662&q=80"
-			/>
+			/> */}
 			<div className="phobias-title d-flex justify-content-center ml-5  ">
 				<p className="part-title">face your </p>
 				<span className="phobia-name ml-3 mr-3"> PHOBIAS</span>
