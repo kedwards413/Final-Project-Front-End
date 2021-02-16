96e4e78a48ffe04345fd8b6fae10faa63c31b3e8
Outdoor-Dev <saltairpix@gmail.com>
2021-02-16 22:49:40 +0000

    update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -14,23 +14,23 @@ export const Home = () => {
 				<span className="phobia-name  "> PHOBIAS</span>
 				<p className="part-title2  ">and be set free</p>
 			</div>
-			<div className="container-fluid d-flex align-content flex-wrap ">
-				<div className="first-card">
+			<div className="container-fluid d-flex align-content ">
+				<div className="first-card hover">
 					<TitleCard title="Arachnophobia" subtitle="FEAR OF SPIDERS" />
 				</div>
-				<div className="second-card">
+				<div className="second-card hover">
 					<TitleCard title="Social Phobia" subtitle="FEAR OF SOCIAL INTERACTION" />
 				</div>
-				<div className="third-card">
+				<div className="third-card hover">
 					<TitleCard title="Acrophobia" subtitle="FEAR OF HEIGHTS" />
 				</div>
-				<div className="fourth-card">
+				<div className="fourth-card hover">
 					<TitleCard title="Cynophobia" subtitle="FEAR OF DOGS" />
 				</div>
-				<div className="fifth-card">
+				<div className="fifth-card hover">
 					<TitleCard title="Claustrophobia" subtitle="FEAR OF ENCLOSED SPACES" />
 				</div>
-				<div className="sixth-card">
+				<div className="sixth-card hover">
 					<TitleCard title=" Astraphobia" subtitle="FEAR OF THUNDER AND LIGHTNING" />
 				</div>
 			</div>
