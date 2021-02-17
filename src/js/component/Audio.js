76dc243b64d21196df16e3baf73d51a008071b05
Outdoor-Dev <saltairpix@gmail.com>
2021-02-17 19:57:01 +0000

    update

--- src/js/component/Audio.js
+++ src/js/component/Audio.js
@@ -1,103 +1,10 @@
-// import React, { useEffect, useState } from "react";
-
-// export const Audio = () => {
-// 	// use Audio constructor to create HTMLAudioElement
-// 	const audioTune = new Audio("../img/sound.mp3");
-
-// 	// variable to play audio in loop
-// 	const [playInLoop, setPlayInLoop] = useState(false);
-
-// 	// load audio file on component load
-// 	useEffect(() => {
-// 		audioTune.load();
-// 	}, []);
-
-// 	// set the loop of audio tune
-// 	useEffect(
-// 		() => {
-// 			audioTune.loop = playInLoop;
-// 		},
-// 		[playInLoop]
-// 	);
-
-// 	// play audio sound
-// 	const playSound = () => {
-// 		audioTune.play();
-// 	};
-
-// 	// pause audio sound
-// 	const pauseSound = () => {
-// 		audioTune.pause();
-// 	};
-
-// 	// stop audio sound
-// 	const stopSound = () => {
-// 		audioTune.pause();
-// 		audioTune.currentTime = 0;
-// 	};
-
-// 	return (
-// 		<div className="App">
-// 			<h3 className="mb-4">
-// 				Play an mp3 file - <a href="../img/sound.mp3">Clue Mediator</a>
-// 			</h3>
-
-// 			<input type="button" className="btn btn-primary mr-2" value="Play" onClick={playSound} />
-// 			<input type="button" className="btn btn-warning mr-2" value="Pause" onClick={pauseSound} />
-// 			<input type="button" className="btn btn-danger mr-2" value="Stop" onClick={stopSound} />
-
-// 			<label>
-// 				<input type="checkbox" checked={playInLoop} onChange={e => setPlayInLoop(e.target.checked)} /> Play in
-// 				Loop
-// 			</label>
-// 		</div>
-// 	);
-// };
-
-// import React, { useEffect } from "react";
-
-// export const Audio = () => {
-// 	// const audioTune = new Audio("../img/sound.mp3");
-// 	// useEffect(() => {
-// 	// 	audioTune.load();
-// 	// }, []);
-// 	const playSound = () => {
-// 		audioTune.play();
-// 	};
-// 	return (
-// 		<div>
-// 			<input type="button" className="btn btn-primary mr-2" value="Play" onClick={playSound} />
-// 			<audio className="audio-element">
-// 				<source src="https://assets.coderrocketfuel.com/pomodoro-times-up.mp3" />
-// 			</audio>
-// 		</div>
-// 	);
-// };
-
-// import React, { useEffect } from "react";
-
-// export const Audio = () => {
-// 	useEffect(() => {
-// 		const audioEl = document.getElementsByClassName("audio-element")[0];
-// 		audioEl.play();
-// 	}, []);
-
-// 	return (
-// 		<div>
-// 			<audio className="audio-element">
-// 				<source src="../img/sound.mp3" />
-// 			</audio>
-// 		</div>
-// 	);
-// };
-
 import useSound from "use-sound";
 import React from "react";
 //include images into your bundle
 import boopSfx from "../../img/sound.mp3";
 
 export const Audio = () => {
-	const [play] = useSound(boopSfx, { volume: 0.02 });
+	const [play] = useSound(boopSfx, { volume: 0.01 });
 
 	return <button onClick={play}>Boop!</button>;
 };
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -16,29 +16,37 @@ const getState = ({ getStore, getActions, setStore }) => {
 			modalCards: [
 				{
 					text:
-						"You're about to begin your journey. There will be missions you need to complete in order to be able to proceed to the next one. The missions will increase in difficulty. If at any time you feel uncomfortable because of an image, you can click on the image and it will turn black. When you're ready you can continue. ",
+						"You're about to begin your journey. There will be missions you need to complete in order to be able to proceed to the next one. The missions will increase in difficulty. If at any time you feel uncomfortable because of an image, you can click on the image and it will turn black. When you're ready you can continue. Caution: next card contains an image ",
 					button: "Click here"
 				},
 				{
-					imageUrl:
-						"https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-					text: "dkaghakdf lafhjakfj  kjojo kjojoj ojoj faosdifj",
-					button: "press here"
+					imageUrl: "https://s.hdnux.com/photos/01/13/13/45/19708276/8/rawImage.jpg",
+					text:
+						"The VAST majority of spiders are not harmful to humans. Instead their webs catch mosqitoes. Mosquitoes kill more humans than any other animal. When you are ready, press 'next' and you will begin your first mission",
+					button: "Next"
+				},
+				{
+					text:
+						" Your first mission is to visit a spider exhibit at a zoo or go to a pet store and watch spiders in an enclosed space. They cannot touch you and will not hurt you. Stay calm and watch them for a bit. Bring a friend for emotional support. Remember to take deep breathes and close your eyes if you need to calm down.  Caution: next card contains an image",
+
+					button: "Next"
+				},
+				{
+					imageUrl: "https://earthsky.org/upl/2002/05/spider_web_between_trees-e1341246131303.jpeg",
+					text:
+						"Great Job at the pet store/zoo! Now it's time to take it a step further. Go outside and look for spiders in their natural habitat. Don't get too close, but just observe them, notice how they act. Caution: next card contains an image.",
+					button: "Next"
 				},
 				{
 					imageUrl:
-						"https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-					text: "dkaghakdf lafhjakfj  kjojo kjojoj ojoj faosdifj",
-					button: "stomp here"
+						"https://media.pri.org/s3fs-public/styles/story_main/public/widow_hand_SeanMcCann-625x450.jpg?itok=MiEpSGq3",
+					text:
+						"Visit a friend that might have a spider or go to a spider exhibit and watch someone else handle a spider. By this point you should feel more comfortable observing this. If you feel overwhelmed, close your eyes and focus on taking deep breathes, you can even repeat positive phrases. ",
+					button: "Next"
 				},
 				{
-					imageUrl:
-						"https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-					text: "dkaghakdf lafhjakfj  kjojo kjojoj ojoj faosdifj",
-					button: "hihi"
-				},
-				{
-					text: "yay! youre not crazy",
+					text:
+						"YOU DID IT! Congratulations! You've come closer than before to overcoming your fear. The more you expose yourself safely to your fears, the stronger you will feel knowing that fear cannot control you. Press the button and sign up for a free one-time session with a therapist! ",
 					button: "see your progress"
 				}
 			]
