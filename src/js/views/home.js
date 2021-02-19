ac278eaf7e11f5da5662a62525a7ca44f853f9f5
Outdoor-Dev <saltairpix@gmail.com>
2021-02-19 03:23:40 +0000

    update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -11,12 +11,25 @@ export const Home = () => {
 
 	return (
 		<>
-			<div className="phobias-title ">
+			<div className="phobia-name">
+				<span id="L">P</span>
+				<span id="T">H</span>
+				<span id="G">O</span>
+				<span id="H">B</span>
+				<span id="T">I</span>
+				<span id="L">A</span>
+				<span id="T">S</span>
+				<span id="I">.</span>
+			</div>
+
+			<div className="first-subtitle">
 				<p className="part-title ">face your fears </p>
-				<span className="phobia-name  "> PHOBIAS</span>
+			</div>
+			<div className="secondsubtitle">
 				<p className="part-title2  ">and be set free</p>
 			</div>
-			<div className="container-fluid d-flex align-content ">
+
+			<div className="container-fluid  ">
 				<div className="first-card">
 					<TitleCard title="Arachnophobia" subtitle="FEAR OF SPIDERS" />
 				</div>
