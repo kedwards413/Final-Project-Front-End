e12f5e02d91fa12a9f373abdd2c6c61c1b2c6c4e
Outdoor-Dev <saltairpix@gmail.com>
2021-02-19 11:02:15 +0000

    update

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,23 +3,21 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar  " style={{ height: 50 }}>
+		<nav className="navbar  " style={{ height: 46 }}>
 			<Link to="/">
-				<span className="navbar-brand text-dark mb-0 h1">fearLESS</span>
+				<span className="navbar-brand text-white  ">fearLESS</span>
 			</Link>
-			<div className="d-flex justify-content-end">
+			<div className="d-flex justify-content-end ">
 				<Link to="/">
-					<span className="navbar-brand text-dark mr-5 ">HOME</span>
+					<span className="navbar-brand  mr-5 text-white">HOME</span>
 				</Link>
 				<Link to="">
-					<span className="navbar-brand text-dark mr-5">ABOUT</span>
+					<span className="navbar-brand  mr-5 text-white">ABOUT</span>
 				</Link>
-				<Link to="">
-					<span className="navbar-brand text-dark mr-5">THERAPISTS</span>
-				</Link>
+
 				<div className="ml-auto">
 					<Link to="/login">
-						<span className="navbar-brand text-dark mr-5">LOGIN</span>
+						<span className="navbar-brand text-white mr-5">LOGIN</span>
 					</Link>
 					<Link to="">
 						<button className="btn btn-dark mr-2 btn-sm mb-1">SIGN UP</button>
