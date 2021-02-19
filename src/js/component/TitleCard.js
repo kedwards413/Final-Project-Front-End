7f3f95e55428e9c88bfea5bd7f97c25f2e8acfca
Outdoor-Dev <saltairpix@gmail.com>
2021-02-19 11:41:33 +0000

    update

--- src/js/component/TitleCard.js
+++ src/js/component/TitleCard.js
@@ -13,10 +13,10 @@ export const TitleCard = props => {
 	return (
 		<>
 			<Link to="/modal" onClick={() => manageModal()} style={{ textDecoration: "none" }}>
-				<div className="card text-center  mr-4  ml-4" style={{ width: 400, height: 200 }}>
+				<div className="card text-center mr-4  ml-4" style={{ width: 400, height: 100 }}>
 					<div className="card-body ">
-						<h5 className="card-title  mt-5">{props.title}</h5>
-						<h6 className="card-subtitle mb-2  ">{props.subtitle}</h6>
+						<h5 className="card-title ">{props.title}</h5>
+						<h6 className="card-subtitle   ">{props.subtitle}</h6>
 					</div>
 				</div>
 			</Link>
