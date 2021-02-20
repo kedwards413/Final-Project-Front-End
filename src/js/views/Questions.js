cdd938e45581157d8523f0b682e5a4c3f57f26d8
kedwards413 <kaela.rose.edwards@gmail.com>
2021-02-20 01:58:57 +0000

    edits to questionnaire

--- src/js/views/Questions.js
+++ src/js/views/Questions.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 export const Questions = () => {
+    const [newAccount, setNewAccount] = useState({
+		phobia: "",
+		severity: "",
+		help: "",
+		goal: ""
+		})
+
 	return (
 		<div className="questions">
 			<div className="container">
@@ -14,33 +21,27 @@ export const Questions = () => {
 								className="form-control"
 								required
 								placeholder="Enter email"
-								onChange={event => setNewContact({ ...newContact, email: event.target.value })}>
+								onChange={event => setNewAccount({ ...newAccount, email: event.target.value })}>
 								<option>Acrophobia - Fear of heights </option>
 								<option>Arachnophobia - Fear of spiders</option>
 								<option>Astraphobia - Fear of thunder and lightning</option>
 								<option>Claustrophobia - Fear of enclosed spaces</option>
-
 								<option>Cynophobia - Fear of dogs</option>
 								<option>Social Phobia - Fear of social interaction</option>
 							</select>
 						</div>
 						<div className="form-group">
-							<label>How severe is your phobia? Please choose between 1-10.</label>
+							<label>How severe is your phobia? Please choose between 1-5</label>
 							<select
 								className="form-control"
 								placeholder="Choose severity"
 								required
-								onChange={event => setNewContact({ ...newContact, email: event.target.value })}>
+								onChange={event => setNewAccount({ ...newAccount, email: event.target.value })}>
 								<option>1 - Mild </option>
 								<option>2</option>
 								<option>3</option>
 								<option>4</option>
-								<option>5</option>
-								<option>6</option>
-								<option>7</option>
-								<option>8</option>
-								<option>9</option>
-								<option>10 - Severe </option>
+								<option>5 - Severe </option>
 							</select>
 						</div>
 						<div className="form-group">
@@ -49,7 +50,7 @@ export const Questions = () => {
 								className="form-control"
 								required
 								placeholder="Yes or no"
-								onChange={event => setNewContact({ ...newContact, email: event.target.value })}>
+								onChange={event => setNewAccount({ ...newAccount, email: event.target.value })}>
 								<option>Yes - with a therapist</option>
 								<option>Yes - through another app</option>
 								<option>No - I have never sought help</option>
@@ -62,14 +63,14 @@ export const Questions = () => {
 								required
 								className="form-control"
 								placeholder="Type here"
-								onChange={event => setNewContact({ ...newContact, address: event.target.value })}
+								onChange={event => setNewAccount({ ...newAccount, address: event.target.value })}
 							/>
 						</div>
 						<button
 							type="button"
 							className="btn btn-primary form-control"
 							onClick={() => {
-								actions.addContact(newContact);
+								actions.addContact(newAccount);
 							}}>
 							Save and create account
 						</button>
