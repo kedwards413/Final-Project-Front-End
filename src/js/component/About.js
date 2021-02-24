a574a42ef7953894467bc9c07209d1e23284de27
Outdoor-Dev <saltairpix@gmail.com>
2021-02-24 13:56:06 +0000

    update

--- src/js/component/About.js
+++ src/js/component/About.js
@@ -14,7 +14,7 @@ export const About = () => {
 
 	return (
 		<>
-			{/* <Scroll/> */}
+			{/* <Scroll /> */}
 			<div className="about-background">
 				<h1 className="aboutTitle"> Our Story </h1>
 				<p className="aboutText ">
--- src/js/component/Scroll.js
+++ src/js/component/Scroll.js
@@ -1,35 +1,35 @@
-// import React from "react";
-// import { About } from "../component/About";
+import React from "react";
+import { About } from "../component/About";
 
-// export function Scroll() {
-// 	const about = React.useRef(null);
-// 	const scrollToBottom = () => {
-// 		about.current.scrollIntoView({
-// 			behavior: "smooth",
-// 			block: "nearest",
-// 			inline: "start"
-// 		});
-// 	};
+export function Scroll() {
+	const about = React.useRef(null);
+	const scrollToBottom = () => {
+		about.current.scrollIntoView({
+			behavior: "smooth",
+			block: "nearest",
+			inline: "start"
+		});
+	};
 
-// 	React.useEffect(
-// 		() => {
-// 			if (about.current) {
-// 				scrollToBottom();
-// 			}
-// 		},
-// 		[about]
-// 	);
+	React.useEffect(
+		() => {
+			if (about.current) {
+				scrollToBottom();
+			}
+		},
+		[about]
+	);
 
-// 	return (
-// 		// Page is overflowing, set height to mimic a full page
-// 		<div className="App" style={{ marginTop: 70, height: 800 }}>
-// 			{/* now set this div to overflow scroll with a small height */}
-// 			<div style={{ overflowY: "scroll", border: "solid" }}>
-// 				<div ref={about} />
-// 			</div>
+	return (
+		// Page is overflowing, set height to mimic a full page
+		<div className="App" style={{ marginTop: 70, height: 800 }}>
+			{/* now set this div to overflow scroll with a small height */}
+			<div style={{ overflowY: "scroll", border: "solid" }}>
+				<div ref={about} />
+			</div>
 
-// 			{/* Set last element within this div to be the ref to scroll to */}
-// 			<About />
-// 		</div>
-// 	);
-// }
+			{/* Set last element within this div to be the ref to scroll to */}
+			<About />
+		</div>
+	);
+}
