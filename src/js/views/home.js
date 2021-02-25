218e8d42a8ec404bf4808e576a4c37495d7b3a77
Outdoor-Dev <saltairpix@gmail.com>
2021-02-25 00:52:58 +0000

    update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -39,7 +39,7 @@ export const Home = () => {
 
 			{/* <div className="fade_rule mt-5" /> */}
 
-			<div className="titleCard-background container-fluid d-flex align-content flex-wrap ">
+			<div className="titleCard-background  container-fluid d-flex align-content flex-wrap ">
 				<TitleCard title="Arachnophobia" subtitle="FEAR OF SPIDERS" />
 				<TitleCard title="Social Phobia" subtitle="FEAR OF SOCIAL INTERACTION" />
 				<TitleCard title="Acrophobia" subtitle="FEAR OF HEIGHTS" />
