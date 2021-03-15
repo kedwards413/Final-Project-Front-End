9d01cc5a3ff49d4fcf34010fab85d5d2c100402c
Outdoor-Dev <saltairpix@gmail.com>
2021-03-15 14:50:41 +0000

    update

--- src/js/views/ProgressBar.js
+++ src/js/views/ProgressBar.js
@@ -1,35 +1,87 @@
-import React, { useState, useContext } from "react";
+// import React, { useState, useContext } from "react";
+// import PropTypes, { string } from "prop-types";
+// import { Context } from "../store/appContext";
+
+// export const Progress = () => {
+// 	const { store, actions } = useContext(Context);
+// 	// console.log(store.modalInfo);
+// 	const [state, setState] = useState(0);
+// 	const isProgressing = store.modalInfo.map(item => item.checkboxes).includes("neutral");
+
+// 	// console.log(isProgressing);
+// 	return (
+// 		<>
+// 			<h2>{state == 99 ? `100% Complete!` : `${state}%`}</h2>
+// 			<ProgressBar width={state} />
+// 			<ProgressButton
+// 				progress={state}
+// 				makeProgress={() => {
+// 					let n = 0;
+// 					for (let i = 0; i < store.modalInfo.length; i++) {
+// 						if (store.modalInfo[i].checkboxes == "neutral") {
+// 							n++;
+// 						}
+// 					}
+
+// 					state < 99 ? setState(isProgressing ? 33 * n : 0) : setState(0);
+// 				}}
+// 			/>
+// 		</>
+// 	);
+// };
+
+// const ProgressBar = ({ width }) => (
+// 	<div className="outer" style={{ height: 50 }}>
+// 		<div
+// 			className="inner"
+// 			style={{
+// 				width: `${width}%`,
+// 				height: 50
+// 			}}
+// 		/>
+// 	</div>
+// );
+
+// const ProgressButton = ({ makeProgress, progress }) => (
+// 	<button className="progressButton " onClick={makeProgress}>
+// 		{progress == 99 ? " Yay! " : "Progress"}
+// 	</button>
+// );
+// (ProgressBar.propTypes = {
+// 	width: PropTypes.string
+// }),
+// 	(ProgressButton.propTypes = {
+// 		makeProgress: PropTypes.string,
+// 		progress: PropTypes.string
+// 	});
+
+import React, { useState, useContext, useEffect } from "react";
 import PropTypes, { string } from "prop-types";
 import { Context } from "../store/appContext";
-
 export const Progress = () => {
 	const { store, actions } = useContext(Context);
-	// console.log(store.modalInfo);
-	const [state, setState] = useState(0);
-	const isProgressing = store.modalInfo.map(item => item.checkboxes).includes("neutral");
 
-	// console.log(isProgressing);
+	const [state, setState] = useState(0);
+	var d = new Date();
+	var month = d.getUTCMonth() + 1;
+	var day = d.getUTCDate();
+	var year = d.getUTCFullYear();
+	var a = month + "/" + day + "/" + year;
+	const progress = store.modalInfo.map(item => item.checkboxes).includes(`neutral - ${a}`);
+	const handleProgress = () => {
+		console.log(store.modalInfo);
+		console.log("state:", state);
+		setState(previous => (state < 99 && progress ? previous + 33 : 0));
+	};
+
 	return (
 		<>
 			<h2>{state == 99 ? `100% Complete!` : `${state}%`}</h2>
 			<ProgressBar width={state} />
-			<ProgressButton
-				progress={state}
-				makeProgress={() => {
-					let n = 0;
-					for (let i = 0; i < store.modalInfo.length; i++) {
-						if (store.modalInfo[i].checkboxes == "neutral") {
-							n++;
-						}
-					}
-
-					state < 99 ? setState(isProgressing ? 33 * n : 0) : setState(0);
-				}}
-			/>
+			<ProgressButton progress={state} makeProgress={handleProgress} />
 		</>
 	);
 };
-
 const ProgressBar = ({ width }) => (
 	<div className="outer" style={{ height: 50 }}>
 		<div
@@ -41,7 +93,6 @@ const ProgressBar = ({ width }) => (
 		/>
 	</div>
 );
-
 const ProgressButton = ({ makeProgress, progress }) => (
 	<button className="progressButton " onClick={makeProgress}>
 		{progress == 99 ? " Yay! " : "Progress"}
