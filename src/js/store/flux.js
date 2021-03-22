4115eba1a6d0964e59e8eb8e7dc9a8efd1bb98fa
Outdoor-Dev <saltairpix@gmail.com>
2021-03-22 10:49:12 +0000

    update

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -44,6 +44,45 @@ const getState = ({ getStore, getActions, setStore }) => {
 			modalCards: [
 				{
 					title: "Arachnophobia",
+					// imageURL: "https://i.pinimg.com/564x/1b/4f/bf/1b4fbfcafd649d3414f1a456e3565087.jpg",
+					text:
+						"You're about to begin your journey. There will be missions you need to complete in order to be able to proceed to the next one. The missions will increase in difficulty. If at any time you feel uncomfortable because of an image, you can click on the image and it will blackout. When you're ready, you can continue. Click the arrow on the bottom to begin your journey. ",
+					button: ""
+				},
+				{
+					title: "Arachnophobia",
+					// imageUrl: "https://s.hdnux.com/photos/01/13/13/45/19708276/8/rawImage.jpg",
+					text:
+						" Spiders save us from the world’s deadliest animal. They mostly eat insects, which helps control their populations. Their webs are particularly adept at catching small flying insects such as mosquitos. Worldwide, mosquito-borne viruses kill more humans than any other animal. Caution: next mission will contain an image of spiders. Feel free to click on the image to black them out.  ",
+					button: ""
+				},
+
+				{
+					title: "Arachnophobia",
+					imageUrl: "https://earthsky.org/upl/2015/10/peacock-spider-e1477522482238.jpg",
+					text:
+						"Each day, try to increase your time spent with the photograph. When you feel safe or comfortable enough, try touching the image. When you are ready, move on to the next mission.",
+
+					button: ""
+				},
+
+				{
+					title: "Arachnophobia",
+					imageUrl: "www.youtube.com/embed/XXkwJupqoJA",
+					text:
+						" A study has found that clips from “Spider-Man” were more effective at curing arachnophobia than footage of the real thing. It reduced participants’ fear-of-spiders by 20 percent.",
+					button: ""
+				},
+				{
+					title: "Arachnophobia",
+					imageUrl:
+						"https://media.pri.org/s3fs-public/styles/story_main/public/widow_hand_SeanMcCann-625x450.jpg?itok=MiEpSGq3",
+					text:
+						"You've reached your last mission! Visit a friend that might have a spider or go to a spider exhibit and watch someone else handle a spider. Try to get as close as you comfortable can and stay for a few long breathes.",
+					button: ""
+				},
+				{
+					title: "Arachnophobia",
 					imageURL: "https://i.pinimg.com/564x/1b/4f/bf/1b4fbfcafd649d3414f1a456e3565087.jpg",
 					text:
 						"You're about to begin your journey. There will be missions you need to complete in order to be able to proceed to the next one. The missions will increase in difficulty. If at any time you feel uncomfortable because of an image, you can click on the image and it will turn black. When you're ready you can continue. Caution: next card contains an image ",
