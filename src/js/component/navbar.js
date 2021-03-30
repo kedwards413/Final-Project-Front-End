d53e98cc240e30884883e19239a968a9b97ce265
Outdoor-Dev <saltairpix@gmail.com>
2021-03-30 21:52:33 +0000

    update

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useRef, myRef, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import { Audio } from "../component/Audio";
 import { Context } from "../store/appContext";
 
@@ -9,7 +9,6 @@ export const Navbar = () => {
 
 	function handleBackClick() {
 		titleRef.current.scrollIntoView({ behavior: "smooth" });
-		// Scroll back to the title element...
 	}
 	const [log, setLog] = useState(false);
 	useEffect(
@@ -22,13 +21,16 @@ export const Navbar = () => {
 		},
 		[store.token]
 	);
-	// const myRef = useRef(null);
-
-	// const executeScroll = () => myRef.current.scrollIntoView();
-	// on about span.   onClick={executeScroll}
+	let location = useLocation().pathname;
+	let history = useHistory();
 	const goToView = () => {
 		const elmnt = document.getElementById("content");
-		elmnt.scrollIntoView();
+		if (location == "/") {
+			elmnt.scrollIntoView({ behavior: "smooth" });
+		} else {
+			history.push("/");
+			if (location == "/") elmnt.scrollIntoView({ behavior: "smooth" });
+		}
 	};
 	return (
 		<nav className="navbar  " style={{ height: 46 }}>
@@ -38,32 +40,32 @@ export const Navbar = () => {
 			<Audio />
 
 			<div className="d-flex justify-content-end ">
-				<Link to="/">
+				<Link to="/" onClick={() => scrollTo(0, 0)}>
 					<span className="navbar-brand  mr-5 ">HOME</span>
 				</Link>
 				<Link onClick={() => goToView()}>
 					<span className="navbar-brand  mr-5 ">ABOUT</span>
 				</Link>
 
-				<Link to="/searchbar">
+				<Link to="/searchbar" onClick={() => scrollTo(0, 0)}>
 					<span className="navbar-brand  mr-5 ">THERAPISTS</span>
 				</Link>
-				<Link to="/articles">
+				<Link to="/articles" onClick={() => scrollTo(0, 0)}>
 					<span className="navbar-brand  mr-5 ">ARTICLES</span>
 				</Link>
 				<div className="ml-auto">
 					{!log ? (
-						<Link to="/login">
+						<Link to="/login" onClick={() => scrollTo(0, 0)}>
 							<span className="navbar-brand  mr-5">LOGIN</span>
 						</Link>
 					) : (
-						<Link to="/">
+						<Link to="/" onClick={() => scrollTo(0, 0)}>
 							<button className="btn btn-dark mr-2 btn-sm mb-1" onClick={() => actions.logout()}>
 								LOGOUT
 							</button>
 						</Link>
 					)}
-					<Link to="/signup">
+					<Link to="/signup" onClick={() => scrollTo(0, 0)}>
 						<button className="btn btn-dark mr-2 btn-sm mb-1">SIGN UP</button>
 					</Link>
 				</div>
